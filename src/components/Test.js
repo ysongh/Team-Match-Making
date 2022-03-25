9ff24a4560eb3ca22adf1d433cabae37f3639aa9
You Song Hou <yousonghou@gmail.com>
2022-03-24 23:28:18 -0400

    feat: install 'ethers' and 'web3modal' and use them to get signature from ETH wallet

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,7 +1,11 @@
 import React, { useEffect } from 'react';
 import { gql } from '@apollo/client';
+import { ethers } from 'ethers';
 
-import { apolloClient } from './ApolloClient';
+import Web3Modal from 'web3modal';
+
+import { apolloClient } from './apollo-client';
+import { generateChallenge } from './generate-challenge'
 
 function Test() {
   useEffect(() => {
@@ -20,10 +24,26 @@ function Test() {
     })
     console.log('Lens example data: ', response)
   }
+
+  const connect = async () => {
+    const web3Modal = new Web3Modal();
+    const connection = await web3Modal.connect();
+    const provider = new ethers.providers.Web3Provider(connection);  
+    console.log(provider);
+
+    const signer = provider.getSigner();
+    console.log(signer);
+    const address = await signer.getAddress();
+    console.log(address);
+
+    const challengeResponse = await generateChallenge(address);
+    const signature = await signer.signMessage(challengeResponse.data.challenge.text)
+    console.log(signature);
+  }
   
   return (
     <div>
-
+      <button onClick={connect}>Connect</button>
     </div>
   )
 }
