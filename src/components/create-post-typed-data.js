d654be0de01b5a486652206c433bf86fa025a3e9
You Song Hou <yousonghou@gmail.com>
2022-03-26 19:17:51 -0400

    feat: create post typed data using LENS API

---
+++ src/components/create-post-typed-data.js
@@ -0,0 +1,45 @@
+import { gql } from '@apollo/client';
+
+import { apolloClient } from './apollo-client';
+
+const CREATE_POST_TYPED_DATA = `
+  mutation($request: CreatePublicPostRequest!) { 
+    createPostTypedData(request: $request) {
+      id
+      expiresAt
+      typedData {
+        types {
+          PostWithSig {
+            name
+            type
+          }
+        }
+      domain {
+        name
+        chainId
+        version
+        verifyingContract
+      }
+      value {
+        nonce
+        deadline
+        profileId
+        contentURI
+        collectModule
+        collectModuleData
+        referenceModule
+        referenceModuleData
+      }
+     }
+   }
+ }
+`
+
+export const createPostTypedData = (createPostTypedDataRequest) => {
+   return apolloClient.mutate({
+    mutation: gql(CREATE_POST_TYPED_DATA),
+    variables: {
+      request: createPostTypedDataRequest
+    },
+  })
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 
 import { explorePublications } from '../components/explore-publications';
+import { createPostTypedData } from '../components/create-post-typed-data';
 
 function Home() {
   const [posts, setPosts] = useState([]);
@@ -20,8 +21,25 @@ function Home() {
     setPosts(_posts.data.explorePublications.items);
   }
 
+  const createPost = async() => {
+    const req = {
+      profileId: "0x0295",
+      contentURI: "ipfs://QmZJTaXfWxRVX33drEYHjKUksrahFdzgZ5pevd94sskLwn.json",
+      collectModule: {
+        revertCollectModule: true
+      },
+      referenceModule: {
+        followerOnlyReferenceModule: false
+      }
+    }
+
+    const _newPost = await createPostTypedData(req);
+    console.log(_newPost);
+  }
+
   return (
     <div className='container'>
+      <button onClick={createPost}>Create Post</button>
       {posts.map(post => (
         <div key={post.id}>
           <h2>
